38873080fcb33652ef509c1bfd014df49542530d
Richard Fenix <richard.fenix@gmail.com>
2017-08-08 18:25:15 -0400

    Added Procfile

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var newData = [];
 
 
 router.get('/api/friends', function(req, res) {
   var dataFile = req.app.get('appData');
   res.json(dataFile);
   });
+
+router.get("/api/new", function(req, res) {
+  res.json(newData);
+});
+
+router.post("/api/new", function(req, res) {
+  var newClient = req.app.get('newData');
+  var myScore = newClient.scores;
+  var total = 0;
+  var bestMatch = 100;
+  var index = -1;
+
+  for(var j=0; j< appData.length; j++){
+    //Iterate through the whole list of friends
+    total = 0;
+
+    for(var i =0; i< myScore.length; i++){
+      //for each friend calculate the total value
+      var dif = Math.abs(myScore[i] - appData[j].scores[i]);
+      total += dif;
+    }
+
+    if(total < bestMatch){
+      bestMatch = total;
+      index = j;
+    }
+
+  }
+
+  console.log('best Choice', buddy[index]);
+  buddy.push(newClient);
+  res.json(buddy[index]);
+});
 
 
 // router.get('/speakers/:speakerid', function(req, res) {
